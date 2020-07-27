5c978fa7965482ec4a55c621ee9b713f4c437836
Talgat Amanbayev <amanbayev@gmail.com>
2020-07-27 23:55:28 +0600

    homework 2 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import './App.css';
 
+let getRandomInt = (max) => {
+  let min = Math.ceil(0);
+  let maxx = Math.floor(max) - 1;
+  let random = Math.floor(Math.random() * (maxx - min + 1)) + min;
+  console.log('getting Random number ' + random);
+  return random;
+};
+
 function App() {
   const [text, setText] = React.useState('abcd');
   const [answer, setAnswer] = React.useState('');
+  const [questionIndex, setQuestionIndex] = React.useState(getRandomInt(3));
+
+  const questionsArray = [
+    {
+      question: "What's the capital of Kazakhstan?",
+      answer: 'Astana',
+    },
+    {
+      question: 'Can you divide by zero?',
+      answer: 'No',
+    },
+    {
+      question: 'Are we there yet?',
+      answer: 'yes',
+    },
+  ];
 
   return (
     <div>
-      <div>What's the capital of Kazakhstan?</div>
+      <div>
+        {questionsArray[questionIndex]
+          ? questionsArray[questionIndex].question
+          : ''}
+      </div>
       <input
         value={text}
         onChange={(event) => {
@@ -24,7 +52,10 @@ function App() {
       </button>
       <button
         onClick={() => {
-          answer === 'Astana' ? alert('Correct!') : alert('Incorrect!!!');
+          answer === questionsArray[questionIndex].answer
+            ? alert('Correct!')
+            : alert('Incorrect!!!');
+          setQuestionIndex(getRandomInt(questionsArray.length));
         }}
       >
         Submit
